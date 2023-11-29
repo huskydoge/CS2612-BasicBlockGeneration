8d84c019d6543097780e3ab084084cd9c8b63175
huskydoge <hbh001098hbh@sjtu.edu.cn>
2023-11-29 20:46:58 +0800

    trial on BB denotations

--- BB_denotations.v
+++ BB_denotations.v
@@ -10,13 +10,30 @@ Local Open Scope string.
 Local Open Scope Z.
 Local Open Scope sets.
 Require Import Main.grammer.
+Require Import Main.cmd_denotations.
 
 
 
 Definition state: Type := var_name -> int64.
 
-Record denote: Type := {
-  nrm: state -> int64 -> Prop;
-  err: state -> Prop;
-}.
+Definition BB_state: Type := nat -> state.
 
+Module BDenote.
+
+Record BDenote: Type := {
+  nrm: BB_state -> BB_state -> Prop;
+  err: BB_state -> Prop;
+  inf: BB_state -> Prop
+}.
+  
+End BDenote.
+
+Definition ujmp_sem (jum_dist: nat): BDenote :=
+  {|
+    nrm := fun s1 s2 =>
+      exists i,
+        D.(nrm) s1 i /\ s2 X = Vint i /\
+        (forall Y, X <> Y -> s2 Y = s1 Y);
+    err := ∅;
+    inf := ∅;
+  |}.
