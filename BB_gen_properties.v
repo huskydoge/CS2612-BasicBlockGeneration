cd76dd3594364363c0c242fb833d3eb86fe9da75
huskydoge <hbh001098hbh@sjtu.edu.cn>
2023-12-27 15:12:38 +0800

    BB_gen_properties

--- BB_gen_properties.v
+++ BB_gen_properties.v
@@ -36,17 +36,42 @@ Then
 
 (*
 Else
-    BBnum' =
-    (list_cmd_BB_gen cmd_BB_gen c2
-        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else BB_num2).(next_block_num) /\
-    BBnow'0.(cmd) = BB_else.(cmd) ++ BBcmds /\
-    BBnow'0.(block_num) = BB_else.(block_num) /\
-    (list_cmd_BB_gen cmd_BB_gen c2
-        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else BB_num2).(BasicBlocks) ++
-    (list_cmd_BB_gen cmd_BB_gen c2
-        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else BB_num2).(BBn) :: nil =
-    (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) ++ (BBnow'0 :: nil) ++ BBs'
-
+     BB_num3 =
+     (list_cmd_BB_gen cmd_BB_gen c2
+        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else
+        BB_num2).(next_block_num) /\
+     BB_now_else.(cmd) = BB_else.(cmd) ++ BB_cmds_else /\
+     BB_now_else.(block_num) = BB_else.(block_num) /\
+     (list_cmd_BB_gen cmd_BB_gen c2
+        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else
+        BB_num2).(BasicBlocks) ++
+     (list_cmd_BB_gen cmd_BB_gen c2
+        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else
+        BB_num2).(BBn) :: nil =
+     (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) ++
+     (BB_now_else :: nil) ++ BBs_else
 *)
 
+(*以生成一次BBs1，之后拿到的下一个用于分配的BBnum继续生成BBs2, 那么两段BBs的number不交*)
 Lemma list_cmd_BB_gen_aux1:
+    forall (BB1 BB2: BasicBlock)(BBnum: nat)(BBlist1 BBlist2: list BasicBlock)(c1 c2: list cmd),
+    let next_block_num := (list_cmd_BB_gen cmd_BB_gen c1 BBlist1 BB1 BBnum).(next_block_num) in
+    let res1 := to_result (list_cmd_BB_gen cmd_BB_gen c1 BBlist1 BB1 BBnum) in
+    let res2 := to_result (list_cmd_BB_gen cmd_BB_gen c2 BBlist2 BB2 next_block_num) in
+    exists BB_delta1 BB_delta2 BB1' BB2',
+    res1 = BBlist1 ++ BB1'::nil ++ BB_delta1 /\
+    res2 = BBlist2 ++ BB2'::nil ++ BB_delta2 /\
+    BBnum_set (BB_delta1) ∩ BBnum_set (BB_delta2) = ∅. (*我这里先不管BB1和BB2的num，因为这个其实对应的就是then和else, 这两个blocknum都好处理*)
+Proof.
+Admitted.
+
+(*生成一段BBs得到的BB*)
+Lemma list_cmd_BB_gen_aux2:
+    forall (BB: BasicBlock) (BBnum: nat) (BBlist: list BasicBlock) (c: list cmd),
+    let next_block_num := (list_cmd_BB_gen cmd_BB_gen c BBlist BB BBnum).(next_block_num) in
+    let res := to_result (list_cmd_BB_gen cmd_BB_gen c BBlist BB BBnum) in
+    exists BB_delta BB',
+    res = BBlist ++ BB'::nil ++ BB_delta /\
+    ~ (BBnum_set (BB_delta) next_block_num).
+Proof.
+Admitted.
