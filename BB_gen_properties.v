d8e673e63540b29c034a0eb59df7d5e532202809
huskydoge <hbh001098hbh@sjtu.edu.cn>
2023-12-27 18:36:16 +0800

    new todo

--- BB_gen_properties.v
+++ BB_gen_properties.v
@@ -75,26 +75,57 @@ SSS BB_next_num_to_asgn: 以BBthen为起点，下一个分配的num
     那么 BBnum_set (BB1::delta1) ∩ BBnumset (BB2::delta2) = ∅
 *)
 
-(*以生成一次BBs1，之后拿到的下一个用于分配的BB_next_num_to_asgn继续生成BBs2, 那么两段BBs的number不交*)
-Lemma list_cmd_BB_gen_aux1:
-    forall (BB1 BB2: BasicBlock)(BB_next_num_to_asgn: nat)(BBlist1 BBlist2: list BasicBlock)(c1 c2: list cmd),
-    let next_block_num := (list_cmd_BB_gen cmd_BB_gen c1 BBlist1 BB1 BB_next_num_to_asgn).(next_block_num) in
-    let res1 := to_result (list_cmd_BB_gen cmd_BB_gen c1 BBlist1 BB1 BB_next_num_to_asgn) in
-    let res2 := to_result (list_cmd_BB_gen cmd_BB_gen c2 BBlist2 BB2 next_block_num) in
-    exists BB_delta1 BB_delta2 BB1' BB2',
-    res1 = BBlist1 ++ BB1'::nil ++ BB_delta1 /\
-    res2 = BBlist2 ++ BB2'::nil ++ BB_delta2 /\
-    BBnum_set (BB_delta1) ∩ BBnum_set (BB_delta2) = ∅. (*我这里先不管BB1和BB2的num，因为这个其实对应的就是then和else, 这两个blocknum都好处理*)
-Proof.
-Admitted.
 
-(*生成一段BBs得到的BB*)
-Lemma list_cmd_BB_gen_aux2:
-    forall (BB: BasicBlock) (BB_next_num_to_asgn: nat) (BBlist: list BasicBlock) (c: list cmd),
-    let next_block_num := (list_cmd_BB_gen cmd_BB_gen c BBlist BB BB_next_num_to_asgn).(next_block_num) in
-    let res := to_result (list_cmd_BB_gen cmd_BB_gen c BBlist BB BB_next_num_to_asgn) in
-    exists BB_delta BB',
-    res = BBlist ++ BB'::nil ++ BB_delta /\
-    ~ (BBnum_set (BB_delta) next_block_num).
+(*BBnumset BBs >= num*)
+Definition BB_all_ge (BBs: list BasicBlock)(num: nat): Prop :=
+    forall BB, In BB BBs -> Nat.le num BB.(block_num).
+(*BBnumset BBs < num*)
+Definition BB_all_lt (BBs: list BasicBlock)(num: nat): Prop :=
+    forall BB, In BB BBs -> Nat.lt BB.(block_num) num.
+
+
+
+Definition P_BBgen_range (startnum endnum: nat)(cmds: list cmd)(BBnow: BasicBlock): Prop :=
+    forall BBs,
+    let res := (list_cmd_BB_gen cmd_BB_gen cmds BBs BBnow startnum) in
+    let basicblocks := to_result res in
+    endnum = res.(next_block_num)
+    /\ 
+    (
+    exists BBnow' BBdelta,
+    basicblocks = BBs ++ BBnow'::nil ++ BBdelta /\
+    BB_all_ge BBdelta startnum /\
+    BB_all_lt BBdelta endnum
+    ).
+
+
+Lemma Q_if_BBgen_range (BB_then BB_else: BasicBlock):
+forall (e: expr) (c1 c2: list cmd)(BBnum: nat)(startnum midnum endnum: nat)(BBs: list BasicBlock),
+    P_BBgen_range startnum midnum c1 BB_then ->
+    P_BBgen_range midnum endnum c2 BB_else ->
+    exists BB_then' BB_then_delta BB_else' BB_else_delta,
+    let then_basicblocks := to_result (list_cmd_BB_gen cmd_BB_gen c1 BBs BB_then startnum) in
+    let else_basicblocks := to_result (list_cmd_BB_gen cmd_BB_gen c2 (BBs ++ BB_then'::nil ++ BB_then_delta) BB_else midnum) in
+    then_basicblocks = BBs ++ BB_then'::nil ++ BB_then_delta /\
+    else_basicblocks = (BBs ++ BB_then'::nil ++ BB_then_delta) ++ BB_else'::nil ++ BB_else_delta /\
+    (*不交*)
+    BBnum_set BB_then_delta ∩ BBnum_set BB_else_delta = ∅.
 Proof.
-Admitted.
+    intros.
+    unfold P_BBgen_range in *.
+    specialize (H BBs).
+    destruct H as [mid_prop H2].
+    destruct H2 as [BB_then' [BB_then_delta [?]]].
+    specialize (H0 (BBs ++ BB_then'::nil ++ BB_then_delta)).
+    destruct H0 as [end_prop H4].
+    destruct H4 as [BB_else' [BB_else_delta [?]]].
+    rename H1 into then_delta_range.
+    rename H2 into else_delta_range.
+    exists BB_then', BB_then_delta, BB_else', BB_else_delta.
+    repeat split.
+    - apply H.
+    - apply H0.
+    - (* #TODO *)
+
+
+
