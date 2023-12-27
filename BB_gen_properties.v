1712c2e2d3da3b976bee6565ffbba5c277bbf538
huskydoge <hbh001098hbh@sjtu.edu.cn>
2023-12-27 22:42:02 +0800

    modified

--- BB_gen_properties.v
+++ BB_gen_properties.v
@@ -16,65 +16,6 @@ Require Import Coq.Lists.List.
 Require Import Main.BB_denotations.
 Require Import Main.BB_aux_proof.
 
-(* Import Denotation.
-Import EDenote.
-Import CDenote.
-Import EmptyEDenote.
-Import BDenote. *)
-
-(*
-Then
-    BB_num2 =
-    (list_cmd_BB_gen cmd_BB_gen c1 (BBs ++ BBnow' :: nil) BB_then BB_num1).(next_block_num) /\
-    BB_now_then.(cmd) = BB_then.(cmd) ++ BB_cmds_then /\
-    BB_now_then.(block_num) = BB_then.(block_num) /\
-    (list_cmd_BB_gen cmd_BB_gen c1 (BBs ++ BBnow' :: nil) BB_then BB_num1).(BasicBlocks) ++
-    (list_cmd_BB_gen cmd_BB_gen c1 (BBs ++ BBnow' :: nil) BB_then BB_num1).(BBn) :: nil = 
-    (BBs ++ BBnow' :: nil) ++ (BB_now_then :: nil) ++ BBs_then 
-
-*)
-
-(*
-Else
-     BB_num3 =
-     (list_cmd_BB_gen cmd_BB_gen c2
-        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else
-        BB_num2).(next_block_num) /\
-     BB_now_else.(cmd) = BB_else.(cmd) ++ BB_cmds_else /\
-     BB_now_else.(block_num) = BB_else.(block_num) /\
-     (list_cmd_BB_gen cmd_BB_gen c2
-        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else
-        BB_num2).(BasicBlocks) ++
-     (list_cmd_BB_gen cmd_BB_gen c2
-        (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) BB_else
-        BB_num2).(BBn) :: nil =
-     (BBs ++ BBnow' :: nil ++ BB_now_then :: nil ++ BBs_then) ++
-     (BB_now_else :: nil) ++ BBs_else
-*)
-
-(*
-BBnow:
-BB_next_num_to_asgn: BBthen的num
-S BB_next_num_to_asgn: BBelse的num
-SS BB_next_num_to_asgn: BBnext的num
-SSS BB_next_num_to_asgn: 以BBthen为起点，下一个分配的num
-*)
-
-(* 
-    - delta1 ∩ delta2 = ∅ ，一条大引理
-    考虑BB1和BB2：
-    - BBnum < BB_next_num_to_asgn -> BBnum < BBnum_set delta ，一条引理
-    - Bnum_set delta ≥ BB_next_num_to_asgn 一条引理，下一个分配的num，一定小于等于新生成的BBs里所有的num
-    - delta.(next_block_num) ⪧ BBnum_set delta 一条引理，而生成完毕后，现在分配到的那个num，一定大于刚刚生成的BBs里所有的num
-    - BBnum < BBnum_set delta -> BBnum ∉ BBnum_set delta 一条引理
-        于是有 BB1num < BBnum_set delta1 < delta1.(next_block_num) = BB_next_num_to_asgn2 ≤ BBnum_set delta2
-        ->
-        BB1num  ∉ BBnum_set delta2
-    - 类似地证明， BB2num  ∉ BBnum_set delta1，但是方向是反一下
-    - 再加上BB1num ≠ BB2 num ，到时候用的时候，这是已经有的条件，不需要引理
-    那么 BBnum_set (BB1::delta1) ∩ BBnumset (BB2::delta2) = ∅
-*)
-
 
 (*BBnumset BBs >= num*)
 Definition BB_all_ge (BBs: list BasicBlock)(num: nat): Prop :=
@@ -110,7 +51,6 @@ Definition Q_BBgen_range (c: cmd): Prop :=
       BB_all_ge BBdelta startnum /\
       BB_all_lt BBdelta endnum
     ).
-
 
 
 Lemma Q_if_BBgen_range:
@@ -153,18 +93,6 @@ Proof.
 Admitted.
 
 
-
-(*TODO 这里需要cmdgen嘛？*)
-Lemma P_BBgen_range_sound:
-  forall (cmds: list cmd) (BBnow: BasicBlock)(BBs: list BasicBlock)(startnum: nat),
-    let res := (list_cmd_BB_gen cmd_BB_gen cmds BBs BBnow startnum) in
-    let endnum := res.(next_block_num) in
-    P_BBgen_range cmd_BB_gen cmds.
-Proof.
-Admitted.
-  
-
-
 Section BB_gen_range_sound.
 
 Variable cmd_BB_gen: cmd -> list BasicBlock -> BasicBlock -> nat -> basic_block_gen_results.
@@ -197,43 +125,6 @@ Fixpoint BB_gen_range_soundness (c: cmd): Q_BBgen_range c :=
 
 
 
-
-
-
-
-
-
-
-
-  
-Lemma Q_if_BBgen_disjoint (BB_then BB_else: BasicBlock):
-forall (e: expr) (c1 c2: list cmd) (BBnum: nat) (startnum midnum endnum: nat) (BBs: list BasicBlock),
-    P_BBgen_range startnum midnum c1 BB_then ->
-    P_BBgen_range midnum endnum c2 BB_else ->
-
-    exists BB_then' BB_then_delta BB_else' BB_else_delta,
-    let then_basicblocks := to_result (list_cmd_BB_gen cmd_BB_gen c1 BBs BB_then startnum) in
-    let else_basicblocks := to_result (list_cmd_BB_gen cmd_BB_gen c2 (BBs ++ BB_then'::nil ++ BB_then_delta) BB_else midnum) in
-    then_basicblocks = BBs ++ BB_then'::nil ++ BB_then_delta /\
-    else_basicblocks = (BBs ++ BB_then'::nil ++ BB_then_delta) ++ BB_else'::nil ++ BB_else_delta /\
-    (*不交*)
-    BBnum_set BB_then_delta ∩ BBnum_set BB_else_delta = ∅.
-Proof.
-    intros.
-    unfold P_BBgen_range in *.
-    specialize (H BBs).
-    destruct H as [mid_prop H2].
-    destruct H2 as [BB_then' [BB_then_delta [?]]].
-    specialize (H0 (BBs ++ BB_then'::nil ++ BB_then_delta)).
-    destruct H0 as [end_prop H4].
-    destruct H4 as [BB_else' [BB_else_delta [?]]].
-    rename H1 into then_delta_range.
-    rename H2 into else_delta_range.
-    exists BB_then', BB_then_delta, BB_else', BB_else_delta.
-    repeat split.
-    - apply H.
-    - apply H0.
-    - (* #TODO *)
 
 
 
